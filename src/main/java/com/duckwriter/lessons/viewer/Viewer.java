91c446e32f6ac2679fdd7361cae5dc514e34c0e5
Emanuel F. Oliveira <emfolg@gmail.com>
2014-11-06 15:32:51 -0200

    Viewer frame seems OK

--- src/main/java/com/duckwriter/lessons/viewer/Viewer.java
+++ src/main/java/com/duckwriter/lessons/viewer/Viewer.java
@@ -1,19 +1,34 @@
 package com.duckwriter.lessons.viewer;
 
+import java.io.File;
 import java.awt.Window;
 import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.MenuBar;
+import java.awt.FileDialog;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 
 public final class Viewer extends WindowAdapter
-    implements Runnable {
+    implements Runnable, ActionListener {
 
     /*
      * Constants
      */
 
     private static final String TITLE = "Viewer - Shapes and Images";
+
+    /*
+     * Private Fields
+     */
+
+    private Frame viewerFrame;
+    private File workingDirectory;
+    private File selectedFile;
 
     /*
      * Constructors
@@ -24,14 +39,119 @@ public final class Viewer extends WindowAdapter
     }
 
     /*
+     * Private Methods
+     */
+
+    private void exit() {
+        Frame frame = this.viewerFrame;
+        if (frame != null) {
+            frame.setVisible(false);
+            frame.dispose();
+            this.viewerFrame = null;
+        }
+    }
+
+    private void showFileDialog() {
+        FileDialog dialog;
+        File file, directory;
+        String filePath, dirPath;
+        Frame frame = this.viewerFrame;
+        if (frame != null) {
+            dialog = new FileDialog(
+                frame,
+                "Please select an image file or an image producer class...",
+                FileDialog.LOAD
+            );
+            directory = this.workingDirectory;
+            if (directory != null) {
+                dialog.setDirectory(directory.getPath());
+            }
+            // displaying a file dialog blocks the calling thread
+            dialog.setVisible(true);
+            dirPath = dialog.getDirectory();
+            filePath = dialog.getFile();
+            if (filePath != null && dirPath != null) {
+                directory = new File(dirPath);
+                file = new File(directory, filePath);
+                if (file.exists() && file.isFile()) {
+                    this.selectedFile = file;
+                    this.workingDirectory = directory;
+                    this.refresh();
+                }
+            }
+        }
+    }
+
+    private void refresh() {
+        // @todo: load file or class
+        System.out.format("Currently Selected File: %s\n\n", this.selectedFile.getPath());
+    }
+
+    private MenuBar getViewerMenuBar() {
+
+        MenuBar mbMain;
+        MenuItem miOpen;
+        MenuItem miExit;
+        Menu mFile;
+
+        // build menu items
+        miOpen = new MenuItem();
+        miOpen.setLabel("Open...");
+        miOpen.setActionCommand("open");
+        miOpen.addActionListener(this);
+        miExit = new MenuItem();
+        miExit.setLabel("Exit");
+        miExit.setActionCommand("exit");
+        miExit.addActionListener(this);
+
+        // build menu
+        mFile = new Menu();
+        mFile.setLabel("File");
+        mFile.add(miOpen);
+        mFile.add(miExit);
+
+        // build menu bar
+        mbMain = new MenuBar();
+        mbMain.add(mFile);
+
+        return mbMain;
+
+    }
+
+    private Frame getViewerFrame() {
+
+        Frame frame = this.viewerFrame;
+        if (frame == null) {
+            frame = new Frame(TITLE);
+            frame.addWindowListener(this);
+            frame.setMenuBar(this.getViewerMenuBar());
+            this.viewerFrame = frame;
+        }
+        return frame;
+
+    }
+
+    /*
      * Runnable Interface
      */
 
     public void run() {
-        Frame frame = new Frame(TITLE);
-        frame.addWindowListener(this);
+        Frame frame = this.getViewerFrame();
         frame.setBounds(10, 10, 480, 320);
         frame.setVisible(true);
+    }
+
+    /*
+     * Action Listener Interface
+     */
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("open")) {
+            this.showFileDialog();
+        } else if (action.equals("exit")) {
+            this.exit();
+        }
     }
 
     /*
@@ -40,9 +160,7 @@ public final class Viewer extends WindowAdapter
 
     @Override
     public void windowClosing(WindowEvent e) {
-        Window w = e.getWindow();
-        w.setVisible(false);
-        w.dispose();
+        this.exit();
     }
 
     /*
--- src/test/java/com/duckwriter/lessons/viewer/ViewerTest.java
+++ src/test/java/com/duckwriter/lessons/viewer/ViewerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class ViewerTest extends Object {
 
     public ViewerTest(){
-        super()
+        super();
     }
 
     @Test
