be6c68338df654438556859141f6429b877fb59e
Emanuel F. Oliveira <emfolg@gmail.com>
2014-11-28 18:13:54 -0200

    ClassDialog: improved implementation...

--- src/main/java/com/duckwriter/lessons/viewer/ClassDialog.java
+++ src/main/java/com/duckwriter/lessons/viewer/ClassDialog.java
@@ -1,14 +1,14 @@
 package com.duckwriter.lessons.viewer;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.awt.Frame;
 import java.awt.Dialog;
 import java.awt.Panel;
 import java.awt.Button;
+import java.awt.Label;
 import java.awt.TextField;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -19,6 +19,7 @@ class ClassDialog extends Dialog
 
     private static final long serialVersionUID = 1L;
 
+    private static final int INPUT_SIZE = 35;
     private static final String DEFAULT_TITLE;
     private static final String ACTION_OK;
     private static final String ACTION_CANCEL;
@@ -26,27 +27,25 @@ class ClassDialog extends Dialog
     private static final Pattern NONBLANK_REGEX;
 
     static {
-        DEFAULT_TITLE = "Please inform a class name...";
+        DEFAULT_TITLE = "Please enter a class name...";
         ACTION_OK = "OK";
         ACTION_CANCEL = "CANCEL";
         EMPTY_STRING = "";
         NONBLANK_REGEX = Pattern.compile("\\s*(\\S+)\\s*");
     }
 
-    private final AtomicBoolean initState;
-    private final AtomicBoolean visibilityState;
-    private final Set<Class<?>> expectedClasses;
+    private final AtomicReference<Class<?>> selectedClassRef;
+    private final AtomicBoolean isBusy;
+    private boolean isNotInitialized;
 
-    private Class<?> previousSelectedClass;
-    private volatile Class<?> selectedClass;
-
+    private String selectedClassName;
     private TextField textField;
 
     ClassDialog(Frame frame, String title) {
         super(frame, title, true);
-        this.initState = new AtomicBoolean(false);
-        this.visibilityState = new AtomicBoolean(false);
-        this.expectedClasses = new HashSet<Class<?>>();
+        this.selectedClassRef = new AtomicReference<Class<?>>();
+        this.isBusy = new AtomicBoolean(false);
+        this.isNotInitialized = true;
     }
 
     ClassDialog(Frame frame) {
@@ -55,9 +54,9 @@ class ClassDialog extends Dialog
 
     private void init() {
 
-        if (this.initState.compareAndSet(false, true)) {
+        if (isNotInitialized) {
             // text field
-            TextField fdTx = new TextField(35);
+            TextField fdTx = new TextField(INPUT_SIZE);
             // button OK
             Button btOk = new Button("OK");
             btOk.setActionCommand(ACTION_OK);
@@ -72,56 +71,39 @@ class ClassDialog extends Dialog
             panel.add(btOk);
             panel.add(btCn);
             // prepare for exhibition
+            this.add(new Label(DEFAULT_TITLE), BorderLayout.NORTH);
             this.add(panel, BorderLayout.CENTER);
             this.pack();
             // update instance variables
             this.textField = fdTx;
+            this.isNotInitialized = false;
         }
 
-        // save selected class and reset it
-        if (this.selectedClass != null) {
-            this.previousSelectedClass = this.selectedClass;
-            this.selectedClass = null;
-        }
-
-        // set text field initial value
-        this.textField.setText(
-            this.previousSelectedClass != null
-                ? this.previousSelectedClass.getName()
-                : EMPTY_STRING
-        );
+        // reset internal logic
+        this.selectedClassName = null;
+        this.selectedClassRef.set(null);
 
     }
 
-    private void loadClassByName(String className) {
+    private void loadSelectedClass() {
 
-        Set<Class<?>> expClsSet;
-        Class<?> selCls = null, ldCls = null;
+        final Pattern pattern = NONBLANK_REGEX;
+        String className = this.selectedClassName;
 
-        if (!className.isEmpty()) {
-            try {
-                ldCls = Class.forName(className);
-            } catch (ClassNotFoundException e) {
-                // TODO: Improve UI for this...
-                System.err.printf("Class \"%s\" could not be found...\n", className);
-            }
-        }
-
-        if (ldCls != null) {
-            expClsSet = this.expectedClasses;
-            if (expClsSet.isEmpty()) {
-                selCls = ldCls;
-            } else {
-                for (Class<?> expCls : expClsSet) {
-                    if (expCls.isAssignableFrom(ldCls)) {
-                        selCls = ldCls;
-                        break;
-                    }
+        if (className != null) {
+            Matcher matcher = pattern.matcher(className);
+            className = matcher.matches() ? matcher.group(1) : EMPTY_STRING;
+            if (!className.isEmpty()) {
+                Class<?> ldCls = null;
+                try {
+                    ldCls = Class.forName(className);
+                } catch (ClassNotFoundException e) {
+                    // TODO: Improve UI for this...
+                    System.err.printf("Class \"%s\" could not be found...\n", className);
                 }
+                this.selectedClassRef.set(ldCls);
             }
         }
-
-        this.selectedClass = selCls;
 
     }
 
@@ -132,15 +114,9 @@ class ClassDialog extends Dialog
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        String text, action;
-        Matcher matcher;
-
-        action = e.getActionCommand();
+        String action = e.getActionCommand();
         if (action.equals(ACTION_OK)) {
-            text = this.textField.getText();
-            matcher = NONBLANK_REGEX.matcher(text);
-            text = matcher.matches() ? matcher.group(1) : EMPTY_STRING;
-            this.loadClassByName(text);
+            this.selectedClassName = this.textField.getText();
             this.setVisible(false);
         } else if (action.equals(ACTION_CANCEL)) {
             this.setVisible(false);
@@ -152,25 +128,26 @@ class ClassDialog extends Dialog
      * Public Methods
      */
 
-    public void addExpectedClass(Class<?> expected) {
-        if (!this.visibilityState.get()) {
-            this.expectedClasses.add(expected);
-        }
-    }
-
     public Class<?> getSelectedClass() {
-        return this.selectedClass;
+        return this.selectedClassRef.get();
     }
 
     @Override
-    public void setVisible(boolean visibility) {
-        boolean expected = !visibility;
-        if (this.visibilityState.compareAndSet(expected, visibility)) {
-            if (visibility) {
+    public void setVisible(final boolean makeVisible) {
+
+        if (makeVisible) {
+            if (this.isBusy.compareAndSet(false, true)) {
                 this.init();
+                super.setVisible(true); // this call blocks...
+                this.loadSelectedClass();
+                this.isBusy.set(false);
             }
-            super.setVisible(visibility);
+        } else {
+            if (this.isBusy.get()) {
+                super.setVisible(false);
+            }
         }
+
     }
 
 }
